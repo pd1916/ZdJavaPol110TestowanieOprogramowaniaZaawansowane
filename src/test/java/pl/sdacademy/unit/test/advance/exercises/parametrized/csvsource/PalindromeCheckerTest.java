53ac8fa6c1f017f287f96a656ee711d3c32b6b32
Pawel Dudko <paweldudko@outlook.com>
2022-05-21 11:34:13 +0200

    solution for @CsvSource

---
+++ src/test/java/pl/sdacademy/unit/test/advance/exercises/parametrized/csvsource/PalindromeCheckerTest.java
@@ -0,0 +1,43 @@
+package pl.sdacademy.unit.test.advance.exercises.parametrized.csvsource;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeCheckerTest {
+
+    /*
+    true:
+    kajak
+    sedes
+    kobyla ma maly bok
+
+
+    false:
+    programowanie
+    java
+    sda
+    "  "
+     */
+
+    // na potrzeby nauki stworzyliśmy jedną metodę testową
+    // ale normalnie powinno się zrobić dwie oddzielne dla true i false
+    @ParameterizedTest
+    @CsvSource({
+            "kajak, true",
+            "sedes, true",
+            "kobyla ma maly bok, true",
+            "programowanie, false",
+            "java, false",
+            "sda, false",
+            "'  ', true"})
+    void shouldVerifyIfStringIsPalindrome(String input, boolean expectedResult) {
+        //when
+        boolean result = PalindromeChecker.isPalindrome(input);
+        //then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+}
