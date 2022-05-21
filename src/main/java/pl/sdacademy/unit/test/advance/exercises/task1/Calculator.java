43ba4c84164788ea558824c8b0546edac4ddbf24
Pawel Dudko <paweldudko@outlook.com>
2022-05-21 15:05:13 +0200

    solution for task1b

--- src/main/java/pl/sdacademy/unit/test/advance/exercises/task1/Calculator.java
+++ src/main/java/pl/sdacademy/unit/test/advance/exercises/task1/Calculator.java
@@ -5,4 +5,11 @@ public class Calculator {
     public long add(int firstDigit, int secondDigit) {
         return (long)firstDigit + secondDigit;
     }
+
+    public double divide(int firstDigit, int secondDigit) {
+        if(secondDigit == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return (double)firstDigit/secondDigit;
+    }
 }
--- src/test/java/pl/sdacademy/unit/test/advance/exercises/task1/CalculatorTest.java
+++ src/test/java/pl/sdacademy/unit/test/advance/exercises/task1/CalculatorTest.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.unit.test.advance.exercises.task1;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -28,6 +30,19 @@ class CalculatorTest {
                 Arguments.of(-1, -3, -4),
                 Arguments.of(Integer.MAX_VALUE, Integer.MAX_VALUE, 4_294_967_294L)
         );
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSecondDigitIsZero() {
+        //given
+        Calculator calculator = new Calculator();
+        //when & then
+        //assertJ
+        assertThatThrownBy(() -> calculator.divide(5, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Cannot divide by zero!");
+        //jUnit
+        assertThrows(IllegalArgumentException.class, () ->calculator.divide(5, 0));
     }
 
 }
